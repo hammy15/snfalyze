c1d127c20978286958d70447780cac80784fa04c
hammy15 <ohamm15@gmail.com>
2025-07-15 14:08:31 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">Welcome to SNFalyze</h1>
+      <p className="mt-4 text-lg">Post-Acute Deal Intelligence</p>
+    </main>
+  );
+}

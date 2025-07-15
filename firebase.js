1fa861fa8865734fd4b0d2369d663c53aa5c3a14
hammy15 <ohamm15@gmail.com>
2025-07-15 14:16:27 -0600

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSy...",
+  authDomain: "snfalyze.firebaseapp.com",
+  projectId: "snfalyze",
+  storageBucket: "snfalyze.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcdef123456",
+};
